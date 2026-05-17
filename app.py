9ad95a52228effb72967545690e7867e44be1325
abdinadifgalde <abdiladifgalde992@gmail.com>
2026-05-17 23:09:14 +0600

    Update app.py

--- app.py
+++ app.py
@@ -205,4 +205,4 @@ def unread_count():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     debug = os.environ.get('FLASK_ENV') == 'development'
-    socketio.run(app, host='0.0.0.0', port=port, debug=debug)
+    socketio.run(app, host='0.0.0.0', port=port, debug=debug, allow_unsafe_werkzeug=True)
